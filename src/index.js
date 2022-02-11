648e0e62a06fc6b8da686526ea6ebe8ba93a7059
Josh Tillinghast <jwtill@icloud.com>
2022-02-11 12:01:18 -0800

    WIP continue to troubleshoot proper parse of api

--- src/index.js
+++ src/index.js
@@ -4,24 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './exchange-service.js';
 
-function convert(amount, currency) {
-  return amount * currency;
-}
+// function createPath(currency) {
+//   let path1 = "response.conversion_rates.";
+//   let fullPath = path1.concat(currency);
+//   return fullPath;
 
-function getElements(response, amount, currency) {
-  const fish = convert(amount, currency);
+// }
+
+function getElements(response, currency, amount) {
+  // const fish = convert(amount, currency);
 
   if (response.result === "success") {
-    $('#show-conversion').text(`Your value in ${currency} is ${fish}`);
+    // let result = createPath(currency);
+    $('#show-conversion').text(`Your ${amount} is worth  ${response.base_code} is *****`);
+
+    // $('#show-conversion').text(`${result}`);
+    // $('#show-conversion').append(`The humidity in ${response.conversion_rates} is ${response.base_code}`);
   } else {
     $('#show-errors').text(`There was an error: ${response} `);
   }
 }
 
 async function makeApiCall(currency, amount) {
-  // console.log(currency, amount);
   const response = await ExchangeService.getRates(currency);
-  getElements(response, amount, currency);
+  getElements(response, currency, amount);
 }
 function clearFields() {
   $("#show-conversion").val("");
