7dc8c6796abd950b2ec421cc3d3a6980c225444e
Josh Tillinghast <jwtill@icloud.com>
2022-02-11 11:16:24 -0800

    Continue to troubleshoot computation and display of api info

--- src/index.js
+++ src/index.js
@@ -4,22 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './exchange-service.js';
 
-function convert(amount, conversionRate) {
-  return amount * conversionRate;
+function convert(amount, currency) {
+  return amount * currency;
 }
 
 function getElements(response, amount, currency) {
-  const conversionRate = response.conversion_rate + amount;
-  conversionRatate
+  const fish = convert(amount, currency);
+
   if (response.result === "success") {
-    $('#show-conversion').text(`Your value in ${currency} is ${convert(amount, conversionRate)}`;
-    
-    
-    
-    // is ${amount}*${response.conversionrate}.${currency}`);
-    
+    $('#show-conversion').text(`Your value in ${currency} is ${fish}`);
   } else {
-    $('#show-errors').text(`There was an error: ${ response } `);
+    $('#show-errors').text(`There was an error: ${response} `);
   }
 }
 
@@ -28,13 +23,13 @@ async function makeApiCall(currency, amount) {
   const response = await ExchangeService.getRates(currency);
   getElements(response, amount, currency);
 }
-function clearFields(){
+function clearFields() {
   $("#show-conversion").val("");
   $("#show-errors").val("");
 }
 
-$(document).ready(function() {
-  $('#convert').click(function() {
+$(document).ready(function () {
+  $('#convert').click(function () {
     clearFields();
     let amount = $('#usd').val();
     let currency = $('#currency').val();
