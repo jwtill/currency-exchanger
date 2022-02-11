7d7a43960ff1e3831c08f7a435b7975d193eda72
Josh Tillinghast <jwtill@icloud.com>
2022-02-11 08:25:23 -0800

    Create exchange-service js with class and async func

---
+++ src/exchange-service.js
@@ -0,0 +1,13 @@
+export default class ExchangeService {  
+  static async getRates(currency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
