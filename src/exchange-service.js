9a2e63d4816e004a21d8a8f1a9b8a527652d2336
Josh Tillinghast <jwtill@icloud.com>
2022-02-12 19:58:08 -0800

    Convert currencies and add styles

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -4,7 +4,6 @@ export default class ExchangeService {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
       if (!response.ok) {
-        // console.log(response.statusText);
         throw Error(response.statusText);
       }
       return response.json();
--- src/index.js
+++ src/index.js
@@ -4,24 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './exchange-service.js';
 
-
-function getElements(response, amount) {
-  if (response.result === "success" && response.error_type != "unsupported-code") {
-    const conversionRate = response.conversion_rates['USD'];
-    clearFields();
+function getElements(rateObj, currencyTo) {
+  if (rateObj.result === "success" && rateObj.error_type != "unsupported-code") {
+    const conversionRate = rateObj.conversion_rates[currencyTo];
+    return conversionRate;
+  }
+  else {
     $('.results').show();
-    $('#show-conversion').text(`Your U.S. $${amount} is worth  ${amount * conversionRate} ${response.base_code}`);
-    $('#show-conversion').text(`Your U.S. $${amount} is worth  ${Math.round((amount * conversionRate)*100)/100} ${response.base_code}`);
-  } else {
-    $('.results').show();
-    $('#show-errors').text(`There was an error: ${response.message} `);
+    $('#show-errors').text(`There was an error: ${rateObj.message} `);
   }
 }
-
-async function makeApiCall(currency, amount) {
-  const response = await ExchangeService.getRates(currency);
-  getElements(response, amount);
+function convertCurrency(conversionRate, amount) {
+  return Math.round((conversionRate * amount)*100)/100;
 }
+function displayElements(conversion, amount, rateObj, currencyTo) {
+  $('#show-conversion').text(`Your ${amount} ${rateObj.base_code} is worth   ${conversion} ${currencyTo}`);
+}
+
+async function makeApiCall(currencyFrom, amount, currencyTo) {
+  const rateObj = await ExchangeService.getRates(currencyFrom);
+  const parsedRate = getElements(rateObj, currencyTo);
+  const conversion = convertCurrency(parsedRate, amount);
+  displayElements(conversion, amount, rateObj, currencyTo);
+}
+
 function clearFields() {
   $("#show-conversion").text("");
   $("#show-errors").text("");
@@ -30,14 +36,16 @@ function clearFields() {
 $(document).ready(function () {
   $('#convert').click(function () {
     clearFields();
-    let amount = $('#usd').val();
-    let currency = $('#currency').val();
+    let amount = $('#amt').val();
+    let currencyFrom = $('#currencyFrom').val();
+    let currencyTo = $('#currencyTo').val().toUpperCase();
     if ((parseInt(amount) <= 0) || (amount === "")) {
       clearFields();
       $('.results').show();
       $('#show-errors').text('Please enter a number greater than 0');
     } else {
-      makeApiCall(currency, amount);
+      $('.results').show();
+      makeApiCall(currencyFrom, amount, currencyTo);
     }
   });
 });
